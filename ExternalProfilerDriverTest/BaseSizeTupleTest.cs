1e94dadb556efc2f757b5459097e3bb39e9d401f
R.G. Esteves <rgesteves@gmail.com>
2018-07-19 23:32:14 -0700

    Trying my hand at testing collections

--- ExternalProfilerDriverTest/BaseSizeTupleTest.cs
+++ ExternalProfilerDriverTest/BaseSizeTupleTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ExternalProfilerDriver;
 
@@ -7,13 +9,20 @@ namespace ExternalProfilerDriverTest
     public class BaseSizeTupleTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestBaseSizeTupleCtor()
         {
             var baseTest = 10;
             var sizeTest = 20;
             var bs = new BaseSizeTuple(baseTest, sizeTest);
 
             Assert.AreEqual(baseTest, bs.Base);
+        }
+
+        [TestMethod]
+        public void TestSequenceBaseSizeGenerate()
+        {
+            var sbs = new SequenceBaseSize();
+            CollectionAssert.AllItemsAreNotNull(sbs.Generate().Take(5).ToList());
         }
     }
 }
