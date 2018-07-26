431845344a33018f99de2867a51c6667c6d2d344
R.G. Esteves <rgesteves@gmail.com>
2018-07-25 22:14:53 -0700

    Split out dictionary-generating functions

--- ExternalProfilerDriverTest/ParseStackTest.cs
+++ ExternalProfilerDriverTest/ParseStackTest.cs
@@ -10,36 +10,6 @@ using ExternalProfilerDriver;
 
 namespace ExternalProfilerDriverTest
 {
-    class FuncInfo
-    {
-        public string FunctionName { get; set; }
-        public string SourceFile { get; set; }
-        public long? Base { get; set; }
-        public long? Size { get; set; }
-
-        public FuncInfo(string _functionname, string _sourceFile = "")
-        {
-            FunctionName = _functionname;
-            SourceFile = _sourceFile;
-            Base = null;
-            Size = null;
-        }
-    }
-    
-    class FuncInfoComparer : IEqualityComparer<FuncInfo>
-    {
-        public bool Equals(FuncInfo x, FuncInfo y)
-        {
-            return x.FunctionName == y.FunctionName && x.SourceFile == y.SourceFile;
-        }
- 
-        public int GetHashCode(FuncInfo obj)
-        {
-            return (obj.FunctionName + obj.SourceFile).GetHashCode();
-        }
-    }
-
-
     [TestClass]
     public class ParseStackTest
     {
@@ -48,7 +18,7 @@ namespace ExternalProfilerDriverTest
         public void TestDeploymentItem()
         {
             string filename = "zlib_example.csv";
-            int expected_sample_count = 5;
+            const int expected_sample_count = 5;
             Assert.IsTrue(File.Exists(filename));
 
             var samples = VTuneToDWJSON.ParseFromFile(filename).ToList();
@@ -56,34 +26,17 @@ namespace ExternalProfilerDriverTest
 
             Assert.IsInstanceOfType(samples[0], typeof(SampleWithTrace));
 
-            var modFunDictionary = samples.SelectMany(sm => sm.AllSamples())
-                                              .Select(p => new { Module = p.Module, Function = p.Function, SourceFile = p.SourceFile })
-                                              .GroupBy(t => t.Module)
-                                              .Select(g => new { Module = g.Key, 
-                                                                 Functions = g.Select(gg => new FuncInfo(gg.Function, gg.SourceFile)).Distinct(new FuncInfoComparer()),
-                                                               });
-            // Two-level dictionary
-            var mfdd = modFunDictionary.ToDictionary(r => r.Module, 
-                                                     r => r.Functions.ToDictionary(
-                                                         rr => rr.FunctionName,
-                                                         rr => rr
-                                                     ));
+            var dict = VTuneToDWJSON.ModuleFuncDictFromSamples(samples);
+            Assert.IsTrue(dict.ContainsKey("libz.so.1"));
+            
+            const int expected_funcs_in_libz = 15;
+            Assert.AreEqual(dict["libz.so.1"].Count, expected_funcs_in_libz);
 
-#if false
-            // Two-level dictionary module -> (function -> (base, size))
-            var mfdd = modFunDictionary.Select(x => new { Module = x.Module,
-                                                          Functions = x.Functions.Zip((new SequenceBaseSize()).Generate(), 
-                                                                                       (f, b) => new { Function = f, BaseSize = b }
-                                                                                     ).ToDictionary(t => t.Function, t => new { BaseSize = t.BaseSize, DefinedIn = 1})
-                                                        }
-                                              ).ToDictionary(od => od.Module, od => od.Functions);
-#endif
-            foreach (var k in mfdd.Keys) {
-                Trace.WriteLine($"#### testing...{k}:");
-                foreach (var f in mfdd[k]) {
-                    Trace.WriteLine($"#### \t{f.Key}");
-                } 
+#if true
+            foreach (var k in dict.Keys) {
+                Trace.WriteLine($"#### from the mod/fun dictionary...{k}: {dict[k].Count}");
             }
+#endif
         }
 
         [TestMethod]
